e00169fc3c09e09527192f4e0846a6dcd6297017
Jonatas Gama <jgama@a5-solutions.local>
2022-02-14 17:56:42 -0300

    first commit

---
+++ src/modules/products/routes/products.routes.ts
@@ -0,0 +1,53 @@
+import { Router } from 'express';
+import ProductController from '../controllers/ProductsController';
+import { celebrate, Joi, Segments } from 'celebrate';
+
+const productsRouter = Router();
+const productController = new ProductController();
+
+productsRouter.get('/', productController.index);
+productsRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.show,
+);
+productsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productController.create,
+);
+productsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.update,
+);
+productsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.delete,
+);
+
+export default productsRouter;
